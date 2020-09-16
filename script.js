27ca5da6dedf39ffbf71e483cc44c280aab8a320
AudieRaymos <audriannakraymos@gmail.com>
2020-09-15 19:54:37 -0700

    Able to start quiz

---
+++ script.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById ('question-container')
+
+startButton.addEventListener('click', startGame)
+
+function startGAme() {
+    
+}
+
+function setNextQuestion() {
+    
+}
+
+function selectAnswer() {
+    
+}
