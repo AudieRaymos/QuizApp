f305a03c86c6cb190c6b506a291e8a6a18b1c08f
AudieRaymos <audriannakraymos@gmail.com>
2020-09-16 16:03:42 -0700

    Questions will now randomize

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById ('question-container')
 
+const shuffledQuestions, currentQuestionIndex
+
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(()=> Math.random()- .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
-    
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function selectAnswer() {
@@ -24,7 +28,6 @@ const question = [
         answers: [
             { text: '4', correct: true }
             { text: '22', correct: false }
-
         ]
     }
 ]
