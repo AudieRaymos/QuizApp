79028a30ae8df3e82dc16e19a27b3a12a57b615d
AudieRaymos <audriannakraymos@gmail.com>
2020-09-16 16:27:47 -0700

    Edit to const and gave a let

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById ('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById ('answer-buttons')
 
-const shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
